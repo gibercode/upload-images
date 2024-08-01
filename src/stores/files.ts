362d8c6b976d8b12899c943fa9dd55c130afd611
gibercode <giberlara@gmail.com>
2024-08-01 14:51:21 -0400

    persistation added

--- src/stores/files.ts
+++ src/stores/files.ts
@@ -1,17 +1,18 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-type UserState = {
-  files: Array<any>;
-  setData: (data: any) => any;
-};
-
 const initialState = {
   files: [],
 };
 
-export const useFileStore = create<UserState>()((set) => ({
-  ...initialState,
-  setData: (data: Array<any>) =>
-    set((state: UserState) => ({ ...state, files: data })),
-}));
+export const useFileStore = create()(
+  persist(
+    (set) => ({
+      ...initialState,
+      setData: (data: any[]) => set(() => ({ files: data })),
+    }),
+    {
+      name: "file",
+    }
+  )
+);
